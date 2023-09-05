69e8df9957c48758ba359f828b4f2f7b80e6eb5b
nimysan <234aini@gmail.com>
2023-09-06 00:28:57 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #git clone https://github.com/nimysan/ChatBotWebUI.git
-
 pip install -r requirements.txt
-cp config-example.json config.json
+#cp config-example.json config.json
 nohup python3 webui.py &
