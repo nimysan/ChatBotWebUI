573f996d3e51c421cb6ae07a1023fc70ddfd9982
nimysan <234aini@gmail.com>
2023-09-27 18:02:25 +0800

    pip install always be killed unexpectly

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,9 @@ if [ ! -f config.json ]
 then
 	cp config-example.json config.json
 fi
+echo "-----------------------"
+echo $PATH
+env
+echo "-----------------------"
 pip install -r requirements.txt --debug --log pip.log
-nohup python3 -u webui.py > webui.log 2>&1
+nohup python3 -u webui.py > webui.log 2>&1 &
