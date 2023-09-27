9c9c424bed8e22768ffa0bcd93fd4021bc3bd512
nimysan <234aini@gmail.com>
2023-09-27 20:48:59 +0800

    use env to see it's ok or not

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -f config.json ]
-then
-	cp config-example.json config.json
+if [ ! -f config.json ]; then
+  cp config-example.json config.json
 fi
 
+export TMPDIR=/home/ec2-user/tmp #防止pip install no-space-left error
 python3 -m venv /home/ec2-user/botenv
 app_env_home=/home/ec2-user/botenv
 
 ${app_env_home}/bin/python3 -m pip install --upgrade pip
-${app_env_home}/bin/pip install -r requirements.txt --debug --log pip.log
-nohup ${app_env_home}/bin/python3 -u webui.py > webui.log 2>&1 &
+${app_env_home}/bin/pip install -r requirements.txt --verbose --log pip.log
+nohup ${app_env_home}/bin/python3 -u webui.py >webui.log 2>&1 &
