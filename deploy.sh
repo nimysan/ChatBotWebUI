d5c64a9dc26d13a94a35f670f21b103e4a680b99
nimysan <234aini@gmail.com>
2023-09-27 21:04:08 +0800

    use env to see it's ok or not

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ python3 -m venv /home/ec2-user/botenv
 app_env_home=/home/ec2-user/botenv
 
 ${app_env_home}/bin/python3 -m pip install --upgrade pip
-${app_env_home}/bin/pip install -r requirements.txt --verbose --log pip.log
+${app_env_home}/bin/pip install --verbose  -r requirements.txt --log pip.log
 nohup ${app_env_home}/bin/python3 -u webui.py >webui.log 2>&1 &
